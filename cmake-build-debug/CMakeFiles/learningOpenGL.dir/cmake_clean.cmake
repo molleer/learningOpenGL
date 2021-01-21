bd05d64103767feb1bf127012b7c007a3310e545
David Möller <damoller99@hotmail.se>
2021-01-21 11:41:18 +0100

    Created a base glfw project which draws a triangle

---
+++ cmake-build-debug/CMakeFiles/learningOpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learningOpenGL.dir/main.cpp.o"
+  "learningOpenGL"
+  "learningOpenGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learningOpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/lib/glfw-3.3.2/examples/CMakeFiles/simple.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/davidm/Documents/projects/learningOpenGL/lib/glfw-3.3.2/deps/glad_gl.c" "/home/davidm/Documents/projects/learningOpenGL/cmake-build-debug/lib/glfw-3.3.2/examples/CMakeFiles/simple.dir/__/deps/glad_gl.c.o"
+  "/home/davidm/Documents/projects/learningOpenGL/lib/glfw-3.3.2/examples/simple.c" "/home/davidm/Documents/projects/learningOpenGL/cmake-build-debug/lib/glfw-3.3.2/examples/CMakeFiles/simple.dir/simple.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../lib/glfw-3.3.2/deps"
+  "../lib/glfw-3.3.2/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/davidm/Documents/projects/learningOpenGL/cmake-build-debug/lib/glfw-3.3.2/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
