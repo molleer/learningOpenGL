a58a6d10e3979f5c22cd7a8374e6a6aaec54b9a7
David Möller <damoller99@hotmail.se>
2021-01-22 14:54:26 +0100

    Switched to another base project

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "GLFW CMake starter", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -18,18 +18,13 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    glClearColor( 0.4f, 0.3f, 0.4f, 0.0f );
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.5f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
